dc8ba549a003889d381c2246013f8ebf9cc84f74
藍 <50108258+kwaa@users.noreply.github.com>
2021-11-25 23:24:23 +0800

    Update widget.tsx

--- pages/widget.tsx
+++ pages/widget.tsx
@@ -1,7 +1,7 @@
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import Head from 'next/head';
 import Script from 'next/script';
-import { ContextType, useContext, useEffect, useState } from 'react';
+import { ContextType, useContext, useEffect, useState, useRef } from 'react';
 import Widget from '../components/Widget';
 import { assertOrigin } from '../lib/config';
 import { ConfigContext, ThemeContext } from '../lib/context';
@@ -93,7 +93,7 @@ export default function WidgetPage({
     emitMetadata,
   });
 
-  const [css, setCSS] = useState('')
+  const ref = useRef(':root {--p: 259 94.4% 51.2%; --pf: 259 94.3% 41%; --pc: 0 0% 100%; --s: 314 100% 47.1%; --sf: 314 100% 37.1%; --sc: 0 0% 100%; --a: 174 60% 51%; --af: 174 59.8% 41%; --ac: 0 0% 100%; --n: 219 14.1% 27.8%; --nf: 222 13.4% 19%; --nc: 0 0% 100%; --b1: 0 0% 100%; --b2: 210 20% 98%; --b3: 216 12.2% 83.9%; --bc: 215 27.9% 16.9%; --in: 207 89.8% 53.9%; --su: 174 100% 29%; --wa: 36 100% 50%; --er: 14 100% 57.1%}');
 
   useEffect(() => {
     const handleMessage = (event: MessageEvent) => {
@@ -111,7 +111,7 @@ export default function WidgetPage({
       }
 
       if ('css' in newConfig) {
-        setCSS(newConfig.css);
+        ref.current = newConfig.css;
         delete newConfig.css;
       }
 
@@ -135,7 +135,7 @@ export default function WidgetPage({
   return (
     <>
       <Head>
-        <style type="text/css">{css}</style>
+        <style type="text/css">{ref.current}</style>
         <base target="_top" />
       </Head>
 
